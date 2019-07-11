3ccf248d05fb01bb9759eb014321289e397f5c74
xwder <xwdercom@gmail.com>
2019-07-12 01:58:15 +0800

    创建 注册中心，一个Eureka Server，两个 Eureka Client

---
+++ xwder-eureka-client02/src/main/java/com/xwder/eureka/EurekaClientApplication.java
@@ -0,0 +1,19 @@
+package com.xwder.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: xwder
+ * @Date: 2019/7/11 21:55
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
---
+++ xwder-eureka-server/src/main/java/com/xwder/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.xwder.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: xwder
+ * @Date: 2019/7/11 21:49
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
