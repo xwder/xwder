1ab80bc6ae938e48c9b0d852fcc6494a62d91c48
xwder <xwdercom@gmail.com>
2020-02-04 02:21:51 +0800

    添加lolmz模块

---
+++ xwder-bizmodule-manage/xwder-manage-admin/src/main/java/com/xwder/manage/modules/lolmz/service/ILolmzPicService.java
@@ -0,0 +1,61 @@
+package com.xwder.manage.modules.lolmz.service;
+
+import com.xwder.manage.modules.lolmz.domain.LolmzPic;
+import java.util.List;
+
+/**
+ * 英雄联盟Service接口
+ * 
+ * @author xwder
+ * @date 2020-02-03
+ */
+public interface ILolmzPicService 
+{
+    /**
+     * 查询英雄联盟
+     * 
+     * @param id 英雄联盟ID
+     * @return 英雄联盟
+     */
+    public LolmzPic selectLolmzPicById(Long id);
+
+    /**
+     * 查询英雄联盟列表
+     * 
+     * @param lolmzPic 英雄联盟
+     * @return 英雄联盟集合
+     */
+    public List<LolmzPic> selectLolmzPicList(LolmzPic lolmzPic);
+
+    /**
+     * 新增英雄联盟
+     * 
+     * @param lolmzPic 英雄联盟
+     * @return 结果
+     */
+    public int insertLolmzPic(LolmzPic lolmzPic);
+
+    /**
+     * 修改英雄联盟
+     * 
+     * @param lolmzPic 英雄联盟
+     * @return 结果
+     */
+    public int updateLolmzPic(LolmzPic lolmzPic);
+
+    /**
+     * 批量删除英雄联盟
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+    public int deleteLolmzPicByIds(String ids);
+
+    /**
+     * 删除英雄联盟信息
+     * 
+     * @param id 英雄联盟ID
+     * @return 结果
+     */
+    public int deleteLolmzPicById(Long id);
+}
