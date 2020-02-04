580907f99d3a2152b7e66c880aadf0a3b46bac89
xwder <xwdercom@gmail.com>
2020-02-05 00:56:50 +0800

    添加精美相册模块

---
+++ xwder-bizmodule-manage/xwder-manage-admin/src/main/java/com/xwder/manage/modules/yt/mapper/ImageYt278comMapper.java
@@ -0,0 +1,61 @@
+package com.xwder.manage.modules.yt.mapper;
+
+import com.xwder.manage.modules.yt.domain.ImageYt278com;
+import java.util.List;
+
+/**
+ * 妹子图图Mapper接口
+ * 
+ * @author xwder
+ * @date 2020-02-04
+ */
+public interface ImageYt278comMapper 
+{
+    /**
+     * 查询妹子图图
+     * 
+     * @param id 妹子图图ID
+     * @return 妹子图图
+     */
+    public ImageYt278com selectImageYt278comById(Long id);
+
+    /**
+     * 查询妹子图图列表
+     * 
+     * @param imageYt278com 妹子图图
+     * @return 妹子图图集合
+     */
+    public List<ImageYt278com> selectImageYt278comList(ImageYt278com imageYt278com);
+
+    /**
+     * 新增妹子图图
+     * 
+     * @param imageYt278com 妹子图图
+     * @return 结果
+     */
+    public int insertImageYt278com(ImageYt278com imageYt278com);
+
+    /**
+     * 修改妹子图图
+     * 
+     * @param imageYt278com 妹子图图
+     * @return 结果
+     */
+    public int updateImageYt278com(ImageYt278com imageYt278com);
+
+    /**
+     * 删除妹子图图
+     * 
+     * @param id 妹子图图ID
+     * @return 结果
+     */
+    public int deleteImageYt278comById(Long id);
+
+    /**
+     * 批量删除妹子图图
+     * 
+     * @param ids 需要删除的数据ID
+     * @return 结果
+     */
+    public int deleteImageYt278comByIds(String[] ids);
+}
